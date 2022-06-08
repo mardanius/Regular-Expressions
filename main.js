d55fb5e13fbaf47ba40b7071f66f3ffb2217d4b5
mardanius <mariojeda.mg@gmail.com>
2022-06-08 13:34:09 -0400

    Match Anything with Wildcard Period

--- main.js
+++ main.js
@@ -185,3 +185,33 @@ You can have multiple flags on your regex like /search/gi */
 let twinkleStar = "Twinkle, twinkle, little star";
 let starRegex = /twinkle/ig; // Change this line
 let result = twinkleStar.match(starRegex); // Change this line
+
+
+
+
+/*---------------------------------
+Match Anything with Wildcard Period
+-----------------------------------
+
+Sometimes you won't (or don't need to) know the exact characters in your patterns. Thinking of all words that 
+match, say, a misspelling would take a long time. Luckily, you can save time using the wildcard character: .
+
+The wildcard character . will match any one character. The wildcard is also called dot and period. You can use 
+the wildcard character just like any other character in the regex. For example, if you wanted to match hug, 
+huh, hut, and hum, you can use the regex /hu./ to match all four words.
+
+let humStr = "I'll hum a song";
+let hugStr = "Bear hug";
+let huRegex = /hu./;
+huRegex.test(humStr);
+huRegex.test(hugStr);
+
+Both of these test calls would return true.
+
+Complete the regex unRegex so that it matches the strings run, sun, fun, pun, nun, and bun. Your regex should 
+use the wildcard character. */
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let result = unRegex.test(exampleStr);
+
